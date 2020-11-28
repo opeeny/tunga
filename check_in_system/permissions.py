6806795de32bec5d0de70a627b54dd5ad069a7bd
opeeny <godfreyopeeny@gmail.com>
2020-11-28 11:08:42 +0300

    update file

--- check_in_system/permissions.py
+++ check_in_system/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permissions to only allow owners of an object to edit
-
+jjj
     """
     def has_object_permission(self, request, view, obj):
         #The read permissions are allowed to any requet
